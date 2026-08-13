0db55fe1325e04d92fdbcdc3dedc7b4ac009bc72
Miłosz Łabędzki <miloszlabedzki7@gmail.com>
2026-08-13 10:30:19 +0200

    Update cennik.php

--- inf03_2023_01_02/cennik.php
+++ inf03_2023_01_02/cennik.php
@@ -50,7 +50,7 @@
 </section>
 
     <footer>
-            <p>Stronę opracował: Miczek</p>
+            <p>Stronę opracował: Miłosz Łabędzki</p>
     </footer>
     <?php mysqli_close($conn)?>
 </body>
